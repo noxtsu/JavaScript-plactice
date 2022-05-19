33554f18e2222e04dddeab14d7f89659b82d0d87
noxtsu <94978861+noxtsu@users.noreply.github.com>
2022-05-19 09:38:51 +0900

    promptStr

--- app2.js
+++ app2.js
@@ -1,7 +1,7 @@
-let alertString;
+let promptStr = prompt('何か好きな文字を入力してください。');
 alertString = addString("WebCamp");
 
-alert(alertString);
+alert(promptStr);
 
 function addString(strA){
   let addStr = "Hello " + strA;
